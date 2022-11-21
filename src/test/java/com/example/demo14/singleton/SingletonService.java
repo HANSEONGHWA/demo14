9f96cd30202bf3a758ce5820354dc0ad9dde93d6
HANSEONGHWA <ghk9206@hanmail.net>
2022-11-22 01:41:31 +0900

    singleton 패턴, singlton 컨테이너

--- src/test/java/com/example/demo14/singleton/SingletonService.java
+++ src/test/java/com/example/demo14/singleton/SingletonService.java
@@ -9,8 +9,9 @@ public class SingletonService {
         return instance;
     }
 
-    //생성자를 pri
+    //생성자를 private으로 선언해서 외부에서 new 키워드를 사용한 객체 생성을 하지 못하게 막음.
     private SingletonService(){
+        System.out.println("싱글톤 객체 로직 호출");
     }
 
 }
--- src/test/java/com/example/demo14/singleton/SingletonTest.java
+++ src/test/java/com/example/demo14/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import com.example.demo14.member.MemberService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,5 +28,39 @@ public class SingletonTest {
 
         //memberService1 =/= memberService2
         assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    //싱글톤 패턴 장점 : 요청이 올 때 마다 객체를 공유해 효율적으로 사용가능.
+    //싱글톤 패턴 단점 : 의존관계상 클라이언트가 구체 클래스에 의존. 테스트하기 어려움. 내부속성 변경 어려움 등.
+    public void singletonService() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 컨테이너와 싱글톤")
+    //스프링 컨테이너 덕분에 요청이 올 때, 이미 만들어진 객체를 공유해서 재사용.
+    // (spring Bean 등록 싱글톤 방식임. 객체 생성 후 반환하기도 함. )
+    public void springContainer() {
+        //AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //1. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService1 =/= memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
