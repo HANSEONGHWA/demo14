3315708df65d204e90507668d53f5b02bed85b67
HANSEONGHWA <ghk9206@hanmail.net>
2022-11-23 17:08:33 +0900

    singleton 패턴, singlton 컨테이너, 싱글톤 방식의 오류, @Configuration 사용

--- src/main/java/com/example/demo14/AppConfig.java
+++ src/main/java/com/example/demo14/AppConfig.java
@@ -27,17 +27,48 @@ public class AppConfig {
 
 
     // MemberServiceImpl을 만들고  MemoryMemberRepository를 사용함. 의존성 주입.
+
+    //* memberRepository singleton test
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+
+    //*ConfigurationSingletonTest 실행 시 예상.
+    //memberService
+    //call AppConfig.memberService 호출, 출력
+    //call AppConfig.memberRepository*
+    //memberRepository
+    //call AppConfig.memberRepository*
+    //orderService
+    //call AppConfig.orderService
+    //call AppConfig.memberRepository*
+    //memberRepository 3번 호출 예측
+
+    //*ConfigurationSingletonTest 실행 후
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //memberRepository가 한 번 호출 됨.
+
+    //AppConfig@CGLIB가 등록되어 싱글톤이 보장됨.
+    //@Configuration을 붙이면 바이트 코드를 조작하는 CGLIB 기술을 사용해서 싱글톤 보장,
+    //@Bean만 적용했을 경우 스프링 빈으로 등록 되지만,  싱글톤이 깨지고 때마다 새로 생성됨.(3번 호출)
+
+    //스프링 설정 정보는 항상  @Configuration 을 사용!!
+
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
         public MemberRepository memberRepository(){
+        System.out.println("call AppConfig.memberRepository");
             return new MemoryMemberRepository();
         }
 
     @Bean
      public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
     @Bean
---
+++ src/test/java/com/example/demo14/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,49 @@
+package com.example.demo14.singleton;
+
+import com.example.demo14.AppConfig;
+import com.example.demo14.member.MemberRepository;
+import com.example.demo14.member.MemberService;
+import com.example.demo14.member.MemberServiceImpl;
+import com.example.demo14.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    // memberService와 orderService에서 참조하는 repository가 같은지? Test.(singleton)
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        // 테스트 메소드 꺼내기 위해 impl로 꺼내봄.
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService에서 참조하는 repository -> memberRepository1 = " + memberRepository1);
+        System.out.println("orderService에서 참조하는 repository -> memberRepository1 = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+        //AppConfig도 Bean에 등록.
+        //임의의 다른 클래스가 singleton이 보장되도록 해줌. (CGLIB)
+        //@Bean 이 붙은 메서드마다 이미 스프링 빈이 존재(스프링 컨테이너에 존재)하면 존재하는 Bean을 반환,
+        //스프링 빈이 없으면 생성해서 스프링 빈으로 반환하는 코드가 동적으로 만들어짐.
+        //덕분에 싱글톤 보장됨.
+
+
+    }
+}
