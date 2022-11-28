86b26466d660a88441769b8cbec0150e63665fc4
HANSEONGHWA <ghk9206@hanmail.net>
2022-11-29 03:44:02 +0900

    컴포넌트 스캔 > 필터

---
+++ src/test/java/com/example/demo14/scan/filter/BeanA.java
@@ -0,0 +1,5 @@
+package com.example.demo14.scan.filter;
+
+@MyIncludeComponent
+public class BeanA {
+}
---
+++ src/test/java/com/example/demo14/scan/filter/BeanB.java
@@ -0,0 +1,5 @@
+package com.example.demo14.scan.filter;
+
+@MyExcludeComponent
+public class BeanB {
+}
