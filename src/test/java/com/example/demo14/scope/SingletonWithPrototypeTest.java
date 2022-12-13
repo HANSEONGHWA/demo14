7344f0b7d35ae2748c674e5d695f2a0753f916ee
HANSEONGHWA <ghk9206@hanmail.net>
2022-12-13 18:11:58 +0900

    빈 스코프(프로토타입 스코프 - 싱글톤 빈과 함께 사용시 문제점.)

--- src/test/java/com/example/demo14/scope/SingletonWithPrototypeTest.java
+++ src/test/java/com/example/demo14/scope/SingletonWithPrototypeTest.java
@@ -2,6 +2,8 @@ package com.example.demo14.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -24,8 +26,57 @@ public class SingletonWithPrototypeTest {
         PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
         prototypeBean2.addCount();
         Assertions.assertThat(prototypeBean2.getCount()).isEqualTo(1);
-
     }
+    //생성시점에 주입되어 한개의 프로토 타입이 사용됨,
+    @Test
+    void singletonClientUserPrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        Assertions.assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        Assertions.assertThat(count2).isEqualTo(2);
+    }
+
+    @Scope("singleton")
+    static class ClientBean {
+        private final PrototypeBean prototypeBean; //생성시점에 주입. X 01
+
+//        @Autowired
+//        ApplicationContext applicationContext;
+
+        @Autowired
+        ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic(){
+//            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class); // 주입시점 마다 생성.(하길 원함.)
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
+
+    @Scope("singleton")
+    static class ClientBean2 {
+        private final PrototypeBean prototypeBean; //생성시점에 주입. X 02 >> prototype 이 각각 생성되지만 사용할 때마다 계속 생성되지 않음.
+
+        @Autowired
+        ClientBean2(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
+
 
     @Scope("prototype")
     static class PrototypeBean {
